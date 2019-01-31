209bb134e90fafd70fbed8902749bc8e0476664d
elena98 <elenapuentes1498@gmail.com>
2019-01-30 20:52:59 -0600

    first commit

---
+++ js/scrip.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    $('.slider').bxSlider({
+        mode:'fade',
+        captions: true,
+        slideWidth:800,
+        auto:true,
+        autoControls:true,
+        stopAutoOnclick:true,
+        speed:100,
+        pause:4000,
+        keyboardEnabled:true
+    });
+});
+
+
+$(document).ready(function(){
+    $('.slider2').bxSlider({
+        mode:'fade',
+        captions: true,
+        slideWidth:450,
+        auto:true,
+        stopAutoOnclick:true,
+        speed:500,
+        pause:4000,
+        keyboardEnabled:true
+    });
+});
+
+
+
